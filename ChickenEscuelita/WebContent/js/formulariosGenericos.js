6c27780ebf9552589ea5e98faf87920e3ac8655b
franery <franery10@gmail.com>
2016-09-19 17:37:11 -0300

    no message

--- ChickenEscuelita/WebContent/js/formulariosGenericos.js
+++ ChickenEscuelita/WebContent/js/formulariosGenericos.js
@@ -1,7 +1,7 @@
 /**
  * 
  */
-function crearMessage(arrayHidden,arrayShown) {
+function crearMensajeModificar(arrayHidden,arrayShown) {
 	var message = '<div class="row">  ' +
     		'<div class="col-md-12"> ' +
     		'<form class="form-horizontal"> ';
@@ -16,51 +16,9 @@ function crearMessage(arrayHidden,arrayShown) {
         	'</div> ';
 	}
 	message += '</form> </div>  </div>';
-	alert(message);
 }
 
 
 function formularioModificar(arrayHidden,arrayShown) {
-	message: crearMessage(arrayHidden,arrayShown),
-	bootbox.dialog({
-		title: "Modificar",
-        buttons: {
-            success: {
-                label: "Save",
-                className: "btn-success",
-//                callback: function (e) {
-//	                var json = { "id" : $('#id').val(), "nombre" :  $('#nombre').val(), "stockHuevos":  $('#stockHuevos').val(),
-//        				 	"stockMaximo": $('#stockMaximo').val(), "borrado": $('#borrado').val()};
-//    	    		var mensaje = document.getElementById("mensajeModificar").value;
-//        		    e.preventDefault();
-//        		    bootbox.confirm(mensaje, function (response) {        
-//	        	        if(response) {
-//	        				table =  $('#tablita').DataTable( {
-//	        					ajax: {
-//	        						url: "depositosModificarJson",
-//	        						type: "POST",
-//	        						data: function(){
-//	        							return JSON.stringify(json);
-//	        						},
-//	        						dataType: "json",
-//	        						contentType: "application/json",
-//	        						processData:false
-//	        					},
-//	        					bDestroy: true,
-//	        					serverside: true,
-//	        					columns: [
-//	        				              { data: "nombre" },
-//	        				              { data: "stockHuevos" },
-//	        				              { data: "stockMaximo" },
-//	        				              {defaultContent:'<button id="borrar">${borrar}</button>'},
-//	        				              {defaultContent:'<button id="modificar">${modificar}</button>'}
-//	        				              ]
-//				        	});
-//				        }
-//			        }); 
-//		        }
-//   	        }
-//		}
-//	});
-});
+	return crearMensajeModificar(arrayHidden,arrayShown);
 }
