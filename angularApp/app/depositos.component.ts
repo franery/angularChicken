ee9adca5e5e5f7f650ae574af656d1f0ea3ed401
ezequiel.a.martin <ezequiel_martin92@hotmail.com>
2016-10-12 16:15:52 -0300

    Boton back en depositos.

--- angularApp/app/depositos.component.ts
+++ angularApp/app/depositos.component.ts
@@ -1,7 +1,19 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'listaDepositos',
-  template: '<h1>Depositos</h1>'
+  template: `
+            <h1>Depositos</h1>
+            <br>
+            <button (click)="goBack()">back</button>
+            `
 })
-export class DepositosComponent { }
+export class DepositosComponent {
+
+    constructor(private location: Location){}
+
+    goBack(): void {
+        this.location.back();
+    }
+}
