239d763b97f4bbcbc6597a0eb15b24ead685ae5c
ezequiel.a.martin <ezequiel_martin92@hotmail.com>
2016-10-17 09:18:35 -0300

    Correcciones.

--- angularApp/app/app.routing.module.ts
+++ angularApp/app/app.routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home.component';
-import { DepositosComponent }  from './depositos.component';
-import { UsuariosComponent }  from './usuarios.component';
-import { GallinerosComponent }  from './gallineros.component';
-import { PerfilesComponent }  from './perfiles.component';
-import { ParametrosComponent }  from './parametros.component';
-import { VentasComponent }  from './ventas.component';
-import { ProveedoresComponent }  from './proveedores.component';
+import { DepositosComponent }  from './depositos/depositos.component';
+import { UsuariosComponent }  from './usuarios/usuarios.component';
+import { GallinerosComponent }  from './gallineros/gallineros.component';
+import { PerfilesComponent }  from './perfiles/perfiles.component';
+import { ParametrosComponent }  from './parametros/parametros.component';
+import { VentasComponent }  from './ventas/ventas.component';
+import { ProveedoresComponent }  from './proveedores/proveedores.component';
 
 @NgModule({
   imports: [
